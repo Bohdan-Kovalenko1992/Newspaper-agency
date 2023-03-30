9366aac9ca84189a79e50a19691b265bf6bc5276
Bohdan Kovalenko <bog7807@gmail.com>
2023-03-30 07:55:36 +0300

    add base and index html, views, urls

--- news/views.py
+++ news/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+from news.models import Newspaper, Redactor, Topic
+
+
+def index(request):
+    num_redactors = Redactor.objects.count()
+    num_newspapers = Newspaper.objects.count()
+    num_topics = Topic.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_redactors": num_redactors,
+        "num_newspapers": num_newspapers,
+        "num_topics": num_topics,
+    }
+    return render(request, "news/index.html", context=context)
+
