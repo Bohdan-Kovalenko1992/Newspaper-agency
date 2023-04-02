919fcc230bcdef6898c58ce2d77949dea9ad8fd2
Bohdan Kovalenko <bog7807@gmail.com>
2023-04-02 07:42:21 +0300

    add views, urls

--- news/views.py
+++ news/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from news.models import Newspaper, Redactor, Topic
 
@@ -17,4 +18,22 @@ def index(request):
         "num_topics": num_topics,
     }
     return render(request, "news/index.html", context=context)
+
+
+class RedactorListView(generic.ListView):
+    model = Redactor
+    queryset = Redactor.objects.all()
+
+
+class NewspaperListView(generic.ListView):
+    model = Newspaper
+    context_object_name = "manufacturer_list"
+    template_name = "news/newspaper_list.html"
+    queryset = Newspaper.objects.all()
+
+
+class TopicListView(generic.ListView):
+    model = Topic
+    template_name = "news/topic_list.html"
+
 
